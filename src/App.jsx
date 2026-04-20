341ddd874141d785095b6cd30ada539e42038d2e
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-04-20 19:53:47 +0800

    feat: menambahkan button dengan menggunakan metode arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,21 @@ function ButtonBlack() {
     );
 }
 
+const ButtonRed = () => {
+    return (
+        <button className="h-10 px-6 font-bold rounded-md bg-red-700 text-white">
+            Beli Sekarang
+        </button>
+    );
+}
+
 function App() {
     return (
         <div className="flex justify-center bg-blue-400 min-h-screen items-center">
             <h1>Hello React!</h1>
             <Button></Button>
             <ButtonBlack></ButtonBlack>
+            <ButtonRed></ButtonRed>
         </div>
     )
 }
