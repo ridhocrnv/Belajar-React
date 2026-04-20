88a479dbb4f107dfc6a1c58ed34a3020164df247
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-04-21 03:41:50 +0800

    feat: menambahkan form input pada App.jsx untuk belajar atomic design

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,49 @@ const Button = (props) => {
     const variants = {
         red: 'bg-red-700',
         green: 'bg-green-700',
-        blue: 'bg-blue-700'
+        blue: 'bg-blue-700',
     }
 
-    const bgColor = variants[props.variant] || 'bg-yellow-300 text-black';
+    const bgColor = variants[props.variant] || 'bg-yellow-300 text-black'
 
     return (
-        <button className={`h-10 px-6 font-bold rounded-md ${bgColor} text-white`}>
+        <button
+            className={`h-10 px-6 font-bold rounded-md ${bgColor} text-white`}
+        >
             {props.children}
         </button>
-    );
+    )
 }
 
 function App() {
     return (
-        <div className="flex justify-center bg-blue-400 min-h-screen items-center gap-x-6">
+        <div className="flex justify-center min-h-screen items-center">
             <h1>Hello React!</h1>
             <Button variant="red">Laravel</Button>
             <Button variant="green">Vue</Button>
             <Button variant="blue">React</Button>
             <Button>Framer Motion</Button>
+            <div className="w-full min-h-xs">
+                <h1 className="text-3xl text-blue-600 font-bold">Login</h1>
+                <p className="font-medium text-slate-600">Silahkan login</p>
+                <form action="">
+                    <div className="mb-5">
+                        <label
+                            htmlFor=""
+                            className="block text-slate-700 text-sm font-bold"
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="text"
+                            className="text-sm border rounded 2-full py-2 px-3 text-slate-700 placeholder:opacity-50"
+                            placeholder='example@email.ac.id'
+                        />
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
 
-export default App;
+export default App
