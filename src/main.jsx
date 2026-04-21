9353eedced7144483c5dd6157e127f6786b0fef1
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-04-21 10:50:14 +0800

    feat: menghapus App.jsx dan menggantikannya dengan components/page/HomePage.jsx dan menambahkan halaman 404 serta mengintegrasikan keduanya pada main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,39 @@
-import React from "react";
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from './App';
-import LoginForm from "./components/forms/LoginForm";
-import RegisterForm from "./components/forms/RegisterForm";
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/pages/HomePage'
+import LoginForm from './components/forms/LoginForm'
+import RegisterForm from './components/forms/RegisterForm'
+import ErrorPage from './components/pages/NotFoundPage'
+import './index.css'
 
 const router = createBrowserRouter([
     {
-        path: "/",
-        element: <div><App></App></div>
+        path: '/',
+        element: (
+            <div>
+                <Home />
+            </div>
+        ),
+        errorElement: <ErrorPage />
     },
     {
-        path: "/login",
-        element: <div><LoginForm></LoginForm></div>
+        path: '/login',
+        element: (
+            <div>
+                <LoginForm></LoginForm>
+            </div>
+        ),
     },
     {
-        path: "/register",
-        element: <div><RegisterForm></RegisterForm></div>
-    }
-]);
+        path: '/register',
+        element: (
+            <div>
+                <RegisterForm></RegisterForm>
+            </div>
+        ),
+    },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
